610e63903b18ec4631bb7f5af6fadd8e020d314c
hseachrist <misterministermaster@gmail.com>
2021-12-06 09:54:14 -0500

    Added some sprite code from obstacles

--- custom_libraries/OBSTACLE.cpp
+++ custom_libraries/OBSTACLE.cpp
@@ -1,6 +1,9 @@
-#include <OBSTACLE.h>
+
 #include <FEHLCD.h>
-#include <Constants.h>
+
+#include "Constants.h"
+#include "Graphics.h"
+#include "OBSTACLE.h"
 
 void Obstacle :: UpdatePosition(){
     setX(getX() - vel/FPS);
@@ -11,5 +14,44 @@ void Obstacle :: UpdatePosition(){
 
 void Obstacle :: Draw(){
     /* Draw as rectangle for now */
+    //setSprite(OS_BIRD_1);
     LCD.DrawRectangle(getX(), getY(), getWidth(), getHeight());
+    //sprite.Draw(getX(), getY());
 }
+
+void Obstacle :: setSprite(ObstacleSprite t) {
+    sprite.Init(obstacle_sprite_list[t], obstacle_sprite_widths[t], obstacle_sprite_heights[t]);
+}
+
+const unsigned char *obstacle_sprite_list[NUM_OBST_SPRITES] {
+    cactus_small_single,
+    cactus_small_double,
+    cactus_small_triple,
+    cactus_big_single,
+    cactus_big_double,
+    cactus_big_quad,
+    bird_1,
+    bird_2
+};
+
+const int obstacle_sprite_widths[NUM_OBST_SPRITES] {
+    CACTUS_SMALL_SINGLE_WIDTH,
+    CACTUS_SMALL_DOUBLE_WIDTH,
+    CACTUS_SMALL_TRIPLE_WIDTH,
+    CACTUS_BIG_SINGLE_WIDTH,
+    CACTUS_BIG_DOUBLE_WIDTH,
+    CACTUS_BIG_QUAD_WIDTH,
+    BIRD_1_WIDTH,
+    BIRD_2_WIDTH
+};
+
+const int obstacle_sprite_heights[NUM_OBST_SPRITES] {
+    CACTUS_SMALL_SINGLE_HEIGHT,
+    CACTUS_SMALL_DOUBLE_HEIGHT,
+    CACTUS_SMALL_TRIPLE_HEIGHT,
+    CACTUS_BIG_SINGLE_HEIGHT,
+    CACTUS_BIG_DOUBLE_HEIGHT,
+    CACTUS_BIG_QUAD_HEIGHT,
+    BIRD_1_HEIGHT,
+    BIRD_2_HEIGHT,
+};
--- custom_libraries/OBSTACLE.h
+++ custom_libraries/OBSTACLE.h
@@ -2,12 +2,33 @@
 #define OBSTACLE_H
 
 #include "OBJECT.h"
-#include <Constants.h>
+#include "Constants.h"
+
+enum ObstacleType {
+    OT_CACTUS,
+    OT_BIRD
+};
+
+enum ObstacleSprite {
+    OS_CACT_SMALL_ONE,
+    OS_CACT_SMALL_DUO,
+    OS_CACT_SMALL_TRI,
+    OS_CACT_BIG_ONE,
+    OS_CACT_BIG_DUO,
+    OS_CACT_BIG_QUAD,
+    OS_BIRD_1,
+    OS_BIRD_2,
+    NUM_OBST_SPRITES
+};
 
 class Obstacle : public Object{
     private:
-        int type;
+        ObstacleType type;
+        ObstacleSprite spriteType;
+        Sprite sprite;
         int vel = 4 * FPS;
+
+        void setSprite(ObstacleSprite);
     public:
         /* Constructor inhereted from Object */
         Obstacle(int w, int h, float x, float y) : Object(w, h, x, y) {}
@@ -16,4 +37,7 @@ class Obstacle : public Object{
         void Draw();
 };
 
+extern const unsigned char *obstacle_sprite_list[NUM_OBST_SPRITES];
+extern const int obstacle_sprite_widths[NUM_OBST_SPRITES];
+extern const int obstacle_sprite_heights[NUM_OBST_SPRITES];
 #endif
